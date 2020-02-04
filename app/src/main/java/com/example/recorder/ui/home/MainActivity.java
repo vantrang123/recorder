6e5ba175e0f14117d5395aee006886fe773048ee
trang123 <duongvantrang11@gmail.com>
2020-02-05 02:33:50 +0700

    permission

--- app/src/main/java/com/example/recorder/ui/home/MainActivity.java
+++ app/src/main/java/com/example/recorder/ui/home/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.recorder.ui.home;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.Menu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.recorder.R;
 import com.example.recorder.ui.settings.SettingActivity;
@@ -18,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private TextView title;
 
+    final int REQUEST_PERMISSION_CODE = 2020;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +37,28 @@ public class MainActivity extends AppCompatActivity {
         setupToolbar();
         setupPager();
 
+        //From android M, need Run-time permission
+        if (checkPermissionFromDevice()) {
+
+        } else {
+            requestPermission();
+        }
     }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[] {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.RECORD_AUDIO
+        }, REQUEST_PERMISSION_CODE);
+    }
+
+    private boolean checkPermissionFromDevice() {
+        int write_external_stogare_result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int record_audio_result = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        return write_external_stogare_result == PackageManager.PERMISSION_GRANTED &&
+                record_audio_result == PackageManager.PERMISSION_GRANTED;
+    }
+
     private void setupToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
         title = (TextView)findViewById(R.id.titleHome);
@@ -66,6 +98,38 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                        grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                    new AlertDialog.Builder(this)
+                            .setTitle("Permission Denied")
+                            .setMessage("Ứng dụng không có quyền quan trọng cần thiết để chạy. Hãy kiểm tra cài đặt quyền của bạn.")
 
+                            /*// Specifying a listener allows you to take an action before dismissing the dialog.
+                            // The dialog is automatically dismissed when a dialog button is clicked.
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // Continue with delete operation
+                                }
+                            })*/
 
+                            // A null listener allows the button to dismiss the dialog and take no further action.
+                            .setNegativeButton("Loại bỏ", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    finish();
+                                }
+                            })
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                }
+                break;
+        }
+    }
 }
